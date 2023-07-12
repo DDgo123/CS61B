d5351d03dc250adae65974381b80eed77fad70f5
DDgo123 <842082907@qq.com>
2023-07-13 00:26:58 +0800

    Update Commit.java

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -60,8 +60,8 @@ public class Commit implements Serializable {
     }
 
     public String getParentsId() {
-        if (parentsId.isEmpty()){
-            return  null;
+        if (parentsId.isEmpty()) {
+            return null;
         }
 
         return parentsId.get(0);
